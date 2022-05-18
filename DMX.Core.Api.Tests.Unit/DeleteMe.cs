6b9069b325678a889d7adb12bc95d7cf6757db84
Michael Mendelsohn <mimendel@microsoft.com>
2022-05-18 16:06:23 -0700

    INFRA: Initialize Unit Test Project

---
+++ DMX.Core.Api.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,12 @@
+// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+namespace DMX.Core.Api.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(true);
+    }
+}
