0e92da924b776298258b939d1de937a73cdc0180
Michael Mendelsohn <mimendel@microsoft.com>
2022-05-19 14:08:06 -0700

    CODE RUB: Removes Global Usings and Uses .Net 7.0

--- DMX.Core.Api.Tests.Unit/DeleteMe.cs
+++ DMX.Core.Api.Tests.Unit/DeleteMe.cs
@@ -2,6 +2,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // ---------------------------------------------------------------
 
+using Xunit;
+
 namespace DMX.Core.Api.Tests.Unit
 {
     public class DeleteMe
