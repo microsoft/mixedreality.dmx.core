d19685d9c8e79c3bed45a7c4a6e99ac55a68b240
Andrew McClelland <anmcclel@microsoft.com>
2022-05-23 15:34:02 -0700

    Create Device and DeviceCommand models that represent expected model from existing Reverb function app API endpoint

---
+++ DMX.Core.Api/Models/Devices/Device.cs
@@ -0,0 +1,24 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+
+namespace DMX.Core.Api.Models.Devices
+{
+    public class Device
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string ReservationId { get; set; }
+        public bool IsEnabled { get; set; }
+        public bool IsConnected { get; set; }
+        public bool IsReserved { get; set; }
+        public DateTimeOffset StatusUpdatedTimeUtc { get; set; }
+        public DateTimeOffset LastActivityTimeUtc { get; set; }
+        public DateTimeOffset ReservationReleaseTimeUtc { get; set; }
+        public IDictionary<string, string> Properties { get; set; }
+        public IList<DeviceCommand> Commands { get; set; }
+    }
+}
