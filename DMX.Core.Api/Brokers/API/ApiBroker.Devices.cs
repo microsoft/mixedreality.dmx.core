71ce93ee3d6f39e24b43112584760819e930f8b0
Andrew McClelland <anmcclel@microsoft.com>
2022-05-23 15:34:30 -0700

    Implement GetAvailableDevices() API endpoint in ApiBroker

---
+++ DMX.Core.Api/Brokers/API/ApiBroker.Devices.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DMX.Core.Api.Models.Devices;
+
+namespace DMX.Core.Api.Brokers.API
+{
+    public partial class ApiBroker
+    {
+        private const string RelativeUrl = "api/GetAvailableDevicesAsync";
+
+        public async ValueTask<List<Device>> GetAvailableDevices() => await this.GetAsync<List<Device>>(RelativeUrl);
+    }
+}
---
+++ DMX.Core.Api/Brokers/API/IApiBroker.Devices.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DMX.Core.Api.Models.Devices;
+
+namespace DMX.Core.Api.Brokers.API
+{
+    public partial interface IApiBroker
+    {
+        ValueTask<List<Device>> GetAvailableDevices();
+    }
+}
