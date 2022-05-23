a704a9d2098f72108128cde9adc30e85809565c3
Andrew McClelland <anmcclel@microsoft.com>
2022-05-23 15:33:09 -0700

    Add IApiBroker & ApiBroker classes with generic GetAsync() method

---
+++ DMX.Core.Api/Brokers/API/ApiBroker.cs
@@ -0,0 +1,38 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using DMX.Core.Api.Models.Configurations;
+using Microsoft.Extensions.Configuration;
+using RESTFulSense.Clients;
+
+namespace DMX.Core.Api.Brokers.API
+{
+    public partial class ApiBroker : IApiBroker
+    {
+        private readonly IRESTFulApiFactoryClient apiClient;
+        private readonly HttpClient httpClient;
+
+        public ApiBroker(HttpClient httpClient, IConfiguration configuration)
+        {
+            this.httpClient = httpClient;
+            this.apiClient = GetApiClient(configuration);
+        }
+
+        private async ValueTask<T> GetAsync<T>(string relativeUrl) => 
+            await this.apiClient.GetContentAsync<T>(relativeUrl);
+
+        private IRESTFulApiFactoryClient GetApiClient(IConfiguration configuration)
+        {
+            LocalConfigurations localConfigurations = configuration.Get<LocalConfigurations>();
+
+            string apiBaseUrl = localConfigurations.ApiConfigurations.Url;
+            this.httpClient.BaseAddress = new Uri(apiBaseUrl);
+
+            return new RESTFulApiFactoryClient(this.httpClient);
+        }
+    }
+}
