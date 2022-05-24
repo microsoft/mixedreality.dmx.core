72d9ecdc94e9c97daf3afba330ee227b78e14e94
Andrew McClelland <anmcclel@microsoft.com>
2022-05-24 16:32:03 -0700

    Create ExternalLabCollection to deserialize response from Reverb Function API

---
+++ DMX.Core.Api/Models/External/ExternalLabs/ExternalLabCollection.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using DMX.Core.Api.Models.Devices.External.ExternalDevices;
+using Newtonsoft.Json;
+
+namespace DMX.Core.Api.Models.External.ExternalLabs
+{
+    public class ExternalLabCollection
+    {
+        [JsonProperty("Data")]
+        public ExternalDevice[] Devices { get; set; }
+    }
+}
