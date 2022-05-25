fd201aad54392b16968bae9797daf26721b265e5
Michael Mendelsohn <mimendel@microsoft.com>
2022-05-24 17:30:03 -0700

    BROKER: Create AppServicePlan

---
+++ DMX.Core.Api.Infrastructure.Provision/Brokers/Clouds/ICloudBroker.Plans.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Azure.Management.AppService.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+
+namespace DMX.Core.Api.Infrastructure.Provision.Brokers.Clouds
+{
+    internal partial interface ICloudBroker
+    {
+        ValueTask<IAppServicePlan> CreatePlanAsync(string planName, IResourceGroup resourceGroup);
+    }
+}
