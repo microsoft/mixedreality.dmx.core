6fbf400e5394900aee8be964914b59d68634ce9b
Hassan Habib <harezk@microsoft.com>
2022-05-25 14:39:05 -0700

    WIP

---
+++ DMX.Core.Api.Tests.Unit/Services/Foundations/LabServiceTests.cs
@@ -0,0 +1,20 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DMX.Core.Api.Brokers.ReverbApis;
+using Moq;
+
+namespace DMX.Core.Api.Tests.Unit.Services.Foundations
+{
+    public partial class LabServiceTests
+    {
+        private readonly Mock<IReverbApiBroker> reverbApiBrokerMock;
+        private readonly Mock<ILoggingBroker>
+    }
+}
---
+++ DMX.Core.Api/Services/Foundations/LabService.cs
@@ -0,0 +1,31 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DMX.Core.Api.Brokers.ReverbApis;
+using DMX.Core.Api.Models.Labs;
+using Microsoft.Extensions.Logging;
+
+namespace DMX.Core.Api.Services.Foundations
+{
+    public class LabService : ILabService
+    {
+        private readonly IReverbApiBroker reverbApiBroker;
+        private readonly ILoggingBuilder loggingBuilder;
+
+        public LabService(
+            IReverbApiBroker reverbApiBroker,
+            ILoggingBuilder loggingBuilder)
+        {
+            this.reverbApiBroker = reverbApiBroker;
+            this.loggingBuilder = loggingBuilder;
+        }
+
+        public ValueTask<List<Lab>> RetrieveAllLabsAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
