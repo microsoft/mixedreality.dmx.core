6ff6321c3877e212749f0a3905304a6b44f2295b
Michael Mendelsohn <mimendel@microsoft.com>
2022-05-24 18:38:02 -0700

    BROKER: Implement Logging Broker

---
+++ DMX.Core.Api.Infrastructure.Provision/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,11 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+namespace DMX.Core.Api.Infrastructure.Provision.Brokers.Loggings
+{
+    public interface ILoggingBroker
+    {
+        void LogActivity(string message);
+    }
+}
---
+++ DMX.Core.Api.Infrastructure.Provision/Brokers/Loggings/LoggingBroker.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+
+namespace DMX.Core.Api.Infrastructure.Provision.Brokers.Loggings
+{
+    public class LoggingBroker : ILoggingBroker
+    {
+        public void LogActivity(string message) =>
+            Console.WriteLine(message);
+    }
+}
