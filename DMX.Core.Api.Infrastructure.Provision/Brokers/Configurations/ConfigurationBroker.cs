7952f6f701a081cbb8024a1acc2cff5e3f0c8b5b
Michael Mendelsohn <mimendel@microsoft.com>
2022-05-24 18:34:42 -0700

    BROKER: Implement Configuration

---
+++ DMX.Core.Api.Infrastructure.Provision/Brokers/Configurations/ConfigurationBroker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DMX.Core.Api.Infrastructure.Provision.Models.Configurations;
+using Microsoft.Extensions.Configuration;
+
+namespace DMX.Core.Api.Infrastructure.Provision.Brokers.Configurations
+{
+    public class ConfigurationBroker : IConfigurationBroker
+    {
+        public CloudManagementConfiguration GetConfiguration()
+        {
+            IConfigurationRoot configrationRoot = new ConfigurationBuilder()
+                .SetBasePath(basePath: Directory.GetCurrentDirectory())
+                    .AddJsonFile(path: "appsettings.json", optional: false)
+                        .Build();
+
+            return configrationRoot.Get<CloudManagementConfiguration>();
+        }
+    }
+}
