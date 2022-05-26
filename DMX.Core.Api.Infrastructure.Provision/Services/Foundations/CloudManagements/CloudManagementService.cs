e96d33a41ace03e878e19921a1069508d2900ee5
Michael Mendelsohn <mimendel@microsoft.com>
2022-05-25 19:04:40 -0700

    FOUNDATIONS: Provision Resource Group

---
+++ DMX.Core.Api.Infrastructure.Provision/Services/Foundations/CloudManagements/CloudManagementService.cs
@@ -0,0 +1,36 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using DMX.Core.Api.Infrastructure.Provision.Brokers.Clouds;
+using DMX.Core.Api.Infrastructure.Provision.Brokers.Loggings;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+
+namespace DMX.Core.Api.Infrastructure.Provision.Services.Foundations.CloudManagements
+{
+    public class CloudManagementService : ICloudManagementService
+    {
+        private readonly ICloudBroker cloudBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public async ValueTask<IResourceGroup> CreateResourceGroupAsync(
+            string projectName,
+            string environment)
+        {
+            string resourceGroupName = $"{projectName}-RESOURCES-{environment}".ToUpper();
+
+            this.loggingBroker.LogActivity(
+                message: $"Starting provisioning {resourceGroupName} ...");
+
+            IResourceGroup resourceGroup =
+                await this.cloudBroker.CreateResourceGroupAsync(
+                    resourceGroupName);
+
+            this.loggingBroker.LogActivity(
+                message: $"Provisioning {resourceGroupName} Completed.");
+
+            return resourceGroup;
+        }
+    }
+}
---
+++ DMX.Core.Api.Infrastructure.Provision/Services/Foundations/CloudManagements/ICloudManagementService.cs
@@ -0,0 +1,10 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+namespace DMX.Core.Api.Infrastructure.Provision.Services.Foundations.CloudManagements
+{
+    public interface ICloudManagementService
+    {
+    }
+}
