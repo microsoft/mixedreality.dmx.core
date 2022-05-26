3aad1841f205dbb78459c8cc628ec97c07f8554a
Michael Mendelsohn <mimendel@microsoft.com>
2022-05-26 16:44:59 -0700

    ShouldThrowServiceExceptionOnRetrieveIfErrorOccursAndLogItAsync -> FAIL

--- DMX.Core.Api.Tests.Unit/Services/Foundations/LabServiceTests.Exceptions.RetrieveAll.cs
+++ DMX.Core.Api.Tests.Unit/Services/Foundations/LabServiceTests.Exceptions.RetrieveAll.cs
@@ -2,6 +2,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // ---------------------------------------------------------------
 
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -91,6 +92,47 @@ namespace DMX.Core.Api.Tests.Unit.Services.Foundations
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogError(It.Is(SameExceptionAs(
                     expectedLabDependencyException))),
+                        Times.Once);
+
+            this.reverbApiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnRetrieveIfErrorOccursAndLogItAsync()
+        {
+            // given
+            var someResponseMessage = new HttpResponseMessage();
+            string someMessage = GetRandomString();
+            var httpResponseException = new HttpResponseException(someResponseMessage, someMessage);
+
+            var failedExternalLabServiceException =
+                new FailedLabServiceException(httpResponseException);
+
+            var expectedLabServiceException =
+                new LabServiceException(failedExternalLabServiceException);
+
+            this.reverbApiBrokerMock.Setup(broker =>
+                broker.GetAvailableDevicesAsync(
+                    It.IsAny<ExternalLabsServiceInformation>()))
+                        .ThrowsAsync(httpResponseException);
+
+            // when
+            ValueTask<List<Lab>> retrieveAllLabsTask =
+                this.labService.RetrieveAllLabsAsync();
+
+            // then
+            await Assert.ThrowsAsync<LabServiceException>(() =>
+                retrieveAllLabsTask.AsTask());
+
+            this.reverbApiBrokerMock.Verify(broker =>
+                broker.GetAvailableDevicesAsync(
+                    It.IsAny<ExternalLabsServiceInformation>()),
+                        Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedLabServiceException))),
                         Times.Once);
 
             this.reverbApiBrokerMock.VerifyNoOtherCalls();
