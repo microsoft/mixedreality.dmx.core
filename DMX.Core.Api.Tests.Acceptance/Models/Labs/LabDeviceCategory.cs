aea4f276ad22bdfe50585c9338c5c9476d8a985d
Andrew McClelland <andrew.mcclelland@microsoft.com>
2022-06-01 16:25:36 -0700

    Add ExternalLabs & Labs models into acceptance test project

---
+++ DMX.Core.Api.Tests.Acceptance/Models/Labs/LabDeviceCategory.cs
@@ -0,0 +1,12 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+namespace DMX.Core.Api.Tests.Acceptance.Models.Labs
+{
+    public enum LabDeviceCategory
+    {
+        Host,
+        Attachment
+    }
+}
