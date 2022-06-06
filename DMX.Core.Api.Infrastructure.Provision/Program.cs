5e1253261a87a1cad8e6b6ca42dfab89ed6974f4
Michael Mendelsohn <mimendel@microsoft.com>
2022-06-05 22:45:01 -0700

    CODE RUB: Implements Cloud Provisioning Main Method

--- DMX.Core.Api.Infrastructure.Provision/Program.cs
+++ DMX.Core.Api.Infrastructure.Provision/Program.cs
@@ -1,3 +1,34 @@
 ﻿using System;
+using System.Threading.Tasks;
+using DMX.Core.Api.Infrastructure.Provision.Models.Storages;
+using DMX.Core.Api.Infrastructure.Provision.Services.Foundations.CloudManagements;
+using Microsoft.Azure.Management.AppService.Fluent;
+using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.Sql.Fluent;
 
-Console.WriteLine("Hello World!");
+public class Program
+{
+    public async static Task Main(string[] args)
+    {
+        var cloudManagementService = new CloudManagementService();
+        var projectName = "DMX-CORE-API";
+        var environment = "DEV";
+
+        IResourceGroup resourceGroup = await cloudManagementService.ProvisionResourceGroupAsync(projectName, environment);
+        IAppServicePlan appServicePlan = await cloudManagementService.ProvisionAppServicePlanAsync(projectName, environment, resourceGroup);
+        ISqlServer sqlServer = await cloudManagementService.ProvisionSqlServerAsync(projectName, environment, resourceGroup);
+
+        SqlDatabase sqlDatabase = await cloudManagementService.ProvisionSqlDatabaseAsync(
+            projectName,
+            environment,
+            sqlServer);
+
+        var webApp = await cloudManagementService.ProvisionWebAppAsync(
+            projectName,
+            environment,
+            sqlDatabase.ConnectionString,
+            appServicePlan,
+            resourceGroup);
+    }
+}
