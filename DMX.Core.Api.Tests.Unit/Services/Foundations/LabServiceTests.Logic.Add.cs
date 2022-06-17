08dd4d83db7c93a22d7a76b2f58349b5bf715cbe
Terry Palomares <tpalomares18@gmail.com>
2022-06-17 16:50:20 -0700

    ShouldAddLabAsync -> FAIL

---
+++ DMX.Core.Api.Tests.Unit/Services/Foundations/LabServiceTests.Logic.Add.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace DMX.Core.Api.Tests.Unit.Services.Foundations
+{
+    public partial class LabServiceTests
+    {
+        [Fact]
+        public async Task ShouldAddLabAsync()
+        {
+            // given
+            var randomLab = CreateRandomLab();
+
+            // when
+            var actualLab = 
+                await this.labService.AddLabAsync(randomLab);
+
+            // then
+            actualLab.Should().NotBeNull();
+        }
+    }
+}
