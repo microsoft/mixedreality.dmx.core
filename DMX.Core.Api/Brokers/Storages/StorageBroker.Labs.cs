b4a368cbc167bfc9afad7095de51e60af2c261fe
Terry Palomares <tpalomares18@gmail.com>
2022-06-17 12:04:29 -0700

    BROKERS: Add InsertLabAsync

--- DMX.Core.Api/Brokers/Storages/StorageBroker.Labs.cs
+++ DMX.Core.Api/Brokers/Storages/StorageBroker.Labs.cs
@@ -2,13 +2,27 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // ---------------------------------------------------------------
 
+using System.Threading.Tasks;
 using DMX.Core.Api.Models.Labs;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace DMX.Core.Api.Brokers.Storages
 {
     public partial class StorageBroker
     {
         public DbSet<Lab> Labs { get; set; }
+
+        public async ValueTask<Lab> InsertLabAsync(Lab lab)
+        {
+            var broker = new StorageBroker(this.configuration);
+
+            EntityEntry<Lab> labEntityEntry =
+                await broker.Labs.AddAsync(lab);
+
+            await broker.SaveChangesAsync();
+
+            return labEntityEntry.Entity;
+        }
     }
 }
