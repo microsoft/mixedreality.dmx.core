f1f47450c4691ee41ea3d051cf21369a92877f4b
Terry Palomares <tpalomares18@gmail.com>
2022-06-21 11:32:18 -0700

    MODELS: Add Lab Service Exceptions

---
+++ DMX.Core.Api/Models/Labs/Exceptions/ExternalLabDependencyException.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace DMX.Core.Api.Models.Labs.Exceptions
+{
+    public class ExternalLabDependencyException : Xeption
+    {
+        public ExternalLabDependencyException(Xeption innerException)
+            : base(message: "External dependency error occurred, contact support.",
+                  innerException)
+        { }
+    }
+}
---
+++ DMX.Core.Api/Models/Labs/Exceptions/ExternalLabServiceException.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace DMX.Core.Api.Models.Labs.Exceptions
+{
+    public class ExternaLabServiceException : Xeption
+    {
+        public ExternaLabServiceException(Xeption innerException)
+            : base(message: "External service error occurred, contact support.",
+                  innerException)
+        { }
+    }
+}
