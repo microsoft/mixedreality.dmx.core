4c11bff7c68dfa583ae0f21f670a3fcbfbe9ba5e
Terry Palomares <tpalomares18@gmail.com>
2022-06-21 11:33:22 -0700

    CODE RUB: Add WIP Test

---
+++ DMX.Core.Api.Tests.Unit/Services/Foundations/Labs/LabServiceTests.Validations.Add.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DMX.Core.Api.Tests.Unit.Services.Foundations.Labs
+{
+    public partial class LabServiceTests
+    {
+        [Fact]
+        public void ShouldThrowValidationExceptionOnAddIfNullAndLogItAsync()
+        {
+            // given
+
+
+            // when
+
+            // then
+        }
+    }
+}
