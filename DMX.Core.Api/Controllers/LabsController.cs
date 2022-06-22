3a7174bcdf0650ba2bd11065b79dbe7848d49bb9
Terry Palomares <tpalomares18@gmail.com>
2022-06-22 12:40:27 -0700

    CONTROLLERS: Add Lab

---
+++ DMX.Core.Api/Controllers/LabsController.cs
@@ -0,0 +1,58 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DMX.Core.Api.Models.Labs.Exceptions;
+using DMX.Core.Api.Models.Labs;
+using DMX.Core.Api.Services.Foundations.Labs;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+using System.Reflection.Metadata;
+
+namespace DMX.Core.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LabsController : RESTFulController
+    {
+        private readonly ILabService labService;
+
+        public LabsController(ILabService externalLabService) =>
+            this.labService = externalLabService;
+
+        [HttpPost]
+        public async ValueTask<ActionResult<Lab>> AddLabAsync(Lab lab)
+        {
+            try
+            {
+                Lab addedLab =
+                    await this.labService.AddLabAsync(lab);
+
+                return Created(addedLab);
+            }
+            catch (LabValidationException labValidationException)
+            {
+                return BadRequest(labValidationException.InnerException);
+            }
+            catch (LabDependencyException labDependencyException)
+            {
+                return Problem(labDependencyException.Message);
+            }
+            catch (LabDependencyValidationException labDependencyValidationException)
+                when (labDependencyValidationException.InnerException is AlreadyExistsLabException)
+            {
+                return Conflict(labDependencyValidationException.Message);
+            }
+            catch (LabDependencyValidationException labDependencyValidationException)
+            {
+                return BadRequest(labDependencyValidationException.InnerException);
+            }
+            catch(LabServiceException labServiceException)
+            {
+                return Problem(labServiceException.Message);
+            }
+        }
+    }
+}
