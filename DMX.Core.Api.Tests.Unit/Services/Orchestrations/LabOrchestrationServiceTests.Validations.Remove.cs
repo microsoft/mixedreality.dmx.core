2c5364b1a8d0d10637048afd2e731d6bb9cb5c7c
Michael Mendelsohn <jazzapple42@gmail.com>
2022-07-26 14:52:14 -0700

    ShouldThrowOrchestrationValidationExceptionOnAddIfLabIdIsInvalidAndLogItAsync -> FAIL

---
+++ DMX.Core.Api.Tests.Unit/Services/Orchestrations/LabOrchestrationServiceTests.Validations.Remove.cs
@@ -0,0 +1,56 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using DMX.Core.Api.Models.Foundations.Labs;
+using DMX.Core.Api.Models.Foundations.Labs.Exceptions;
+using DMX.Core.Api.Models.Orchestrations.Labs.Exceptions;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace DMX.Core.Api.Tests.Unit.Services.Orchestrations
+{
+    public partial class LabOrchestrationServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowOrchestrationValidationExceptionOnAddIfLabIdIsInvalidAndLogItAsync()
+        {
+            // given
+            var inputGuid = Guid.Empty;
+            var invalidLabIdException =
+                new InvalidLabIdException(inputGuid);
+
+            var expectedLabOrchestrationValidationException =
+                new LabOrchestrationValidationException(invalidLabIdException);
+
+            // when
+            ValueTask<Lab> actualLabTask =
+                this.labOrchestrationService.RemoveLabByIdAsync(inputGuid);
+
+            LabOrchestrationValidationException
+                actualLabOrchestrationValidationException =
+                    await Assert.ThrowsAsync<LabOrchestrationValidationException>(
+                        actualLabTask.AsTask);
+
+            // then
+            actualLabOrchestrationValidationException.Should()
+                .BeEquivalentTo(expectedLabOrchestrationValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is( SameExceptionAs(
+                    expectedLabOrchestrationValidationException))),
+                        Times.Once);
+
+            this.labServiceMock.Verify(service =>
+                service.RemoveLabByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.labServiceMock.VerifyNoOtherCalls();
+            this.externalLabServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
