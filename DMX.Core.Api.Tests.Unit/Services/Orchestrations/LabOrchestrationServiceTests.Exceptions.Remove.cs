4339b0de45b1320b70bcfd6f42edd3f7de2b2d12
Terry Palomares <tpalomares18@gmail.com>
2022-07-27 10:13:05 -0700

    ShouldThrowOrchestrationDependencyValidationExceptionOnRemoveIfDependencyValidationErrorOccursAndLogItAsync -> PASS

---
+++ DMX.Core.Api.Tests.Unit/Services/Orchestrations/LabOrchestrationServiceTests.Exceptions.Remove.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DMX.Core.Api.Models.Foundations.Labs;
+using DMX.Core.Api.Models.Orchestrations.Labs.Exceptions;
+using FluentAssertions;
+using Moq;
+using Xeptions;
+using Xunit;
+
+namespace DMX.Core.Api.Tests.Unit.Services.Orchestrations
+{
+    public partial class LabOrchestrationServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(LabDependencyValidationExceptions))]
+        public async Task ShouldThrowOrchestrationDependencyValidationExceptionOnRemoveIfDependencyValidationErrorOccursAndLogItAsync(
+            Xeption validationException)
+        {
+            // given
+            Guid randomGuid = Guid.NewGuid();
+            Lab randomLab = CreateRandomLab();
+            Lab someLab = randomLab;
+
+            var expectedLabOrchestrationDependencyValidationException =
+                new LabOrchestrationDependencyValidationException(
+                    validationException.InnerException as Xeption);
+
+            this.labServiceMock.Setup(service =>
+                service.RemoveLabByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(validationException);
+
+            // when
+            ValueTask<Lab> actualLabTask =
+                this.labOrchestrationService.RemoveLabByIdAsync(randomGuid);
+
+            LabOrchestrationDependencyValidationException
+                actualLabOrchestrationDependencyValidationException =
+                    await Assert.ThrowsAsync<LabOrchestrationDependencyValidationException>(
+                        actualLabTask.AsTask);
+
+            // then
+            actualLabOrchestrationDependencyValidationException.Should()
+                .BeEquivalentTo(expectedLabOrchestrationDependencyValidationException);
+
+            this.labServiceMock.Verify(service =>
+                service.RemoveLabByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedLabOrchestrationDependencyValidationException))),
+                        Times.Once);
+
+            this.labServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.externalLabServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
