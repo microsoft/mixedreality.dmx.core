4ac1277b5be6390cf46b8a331fde2c4360feab7c
Michael Mendelsohn <jazzapple42@gmail.com>
2022-08-10 12:22:58 -0700

    BROKERS: Get DateTime

---
+++ DMX.Core.Api/Brokers/DateTimes/IDateTimeBroker.cs
@@ -0,0 +1,13 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+
+namespace DMX.Core.Api.Brokers.DateTimes
+{
+    public interface IDateTimeBroker
+    {
+        public DateTimeOffset GetCurrentDateTime(); 
+    }
+}
