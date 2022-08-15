97ccb76651841cc4af255c1c20f2d2448f249f5e
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2022-08-15 10:54:50 -0700

    CODE RUB: Remove extra line

--- DMX.Core.Api/Brokers/Storages/IStorageBroker.LabCommands.cs
+++ DMX.Core.Api/Brokers/Storages/IStorageBroker.LabCommands.cs
@@ -11,7 +11,6 @@ namespace DMX.Core.Api.Brokers.Storages
     public partial interface IStorageBroker
     {
         ValueTask<LabCommand> InsertLabCommandAsync(LabCommand labCommand);
-
         ValueTask<LabCommand> SelectLabCommandByIdAsync(Guid labCommandId);
     }
 }
