42a6100d43e5d5e6fd7d479dd2806ca5d09d224f
Geetika Jain <geetsk2194@gmail.com>
2022-08-15 10:15:26 -0700

    BROKERS: Update Lab Command

--- DMX.Core.Api/Brokers/Storages/StorageBroker.LabCommands.cs
+++ DMX.Core.Api/Brokers/Storages/StorageBroker.LabCommands.cs
@@ -2,13 +2,27 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // ---------------------------------------------------------------
 
+using System.Threading.Tasks;
 using DMX.Core.Api.Models.Foundations.LabCommands;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace DMX.Core.Api.Brokers.Storages
 {
     public partial class StorageBroker
     {
         public DbSet<LabCommand> LabCommands { get; set; }
+
+        public async ValueTask<LabCommand> UpdateLabCommand(LabCommand command)
+        {
+            var broker = new StorageBroker(this.configuration);
+
+            EntityEntry<LabCommand> labCommandEntityEntry =
+                broker.LabCommands.Update(command);
+
+            await broker.SaveChangesAsync();
+
+            return labCommandEntityEntry.Entity;
+        }
     }
 }
