0669705eaf460e187fb5ff5333ad7010020f4d7e
Terry Palomares <tpalomares18@gmail.com>
2022-08-15 10:28:14 -0700

    BROKERS: InsertLabCommand

--- DMX.Core.Api/Brokers/Storages/StorageBroker.LabCommands.cs
+++ DMX.Core.Api/Brokers/Storages/StorageBroker.LabCommands.cs
@@ -2,13 +2,27 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // ---------------------------------------------------------------
 
+using System.Threading.Tasks;
 using DMX.Core.Api.Models.Foundations.LabCommands;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace DMX.Core.Api.Brokers.Storages
 {
     public partial class StorageBroker
     {
         public DbSet<LabCommand> LabCommands { get; set; }
+
+        public async ValueTask<LabCommand> InsertLabCommand(LabCommand command)
+        {
+            var broker = new StorageBroker(this.configuration);
+
+            EntityEntry<LabCommand> labCommandEntityEntry =
+                await broker.LabCommands.AddAsync(command);
+
+            await broker.SaveChangesAsync();
+
+            return labCommandEntityEntry.Entity;
+        }
     }
 }
