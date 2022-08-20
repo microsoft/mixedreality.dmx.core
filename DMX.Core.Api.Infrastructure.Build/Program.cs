8c0cfd4e51dcd8612d5594000fcafaadd697fbfa
HARPHUNA <109001160+HARPHUNA@users.noreply.github.com>
2022-08-20 20:39:27 +0200

    Delete DMX.Core.Api.Infrastructure.Build directory
    Signed-off-by: HARPHUNA <109001160+HARPHUNA@users.noreply.github.com>

--- DMX.Core.Api.Infrastructure.Build/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// ---------------------------------------------------------------
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// ---------------------------------------------------------------
-
-using DMX.Core.Api.Infrastructure.Build.Services.ScriptGenerations;
-
-var scriptGenerationService = new ScriptGenerationService();
-scriptGenerationService.GenerateBuildScript();
-scriptGenerationService.GenerateProvisionScript();
