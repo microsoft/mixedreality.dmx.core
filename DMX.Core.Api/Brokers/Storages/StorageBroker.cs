b0d310f40d1f86ce38168857c1068464ea83f82f
Geetika Jain <geetsk2194@gmail.com>
2022-09-26 10:33:06 -0700

    CODERUB: Update Storage Broker as per New Standard

--- DMX.Core.Api/Brokers/Storages/StorageBroker.cs
+++ DMX.Core.Api/Brokers/Storages/StorageBroker.cs
@@ -2,6 +2,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // ---------------------------------------------------------------
 
+using System.Linq;
+using System.Threading.Tasks;
 using EFxceptions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -18,8 +20,39 @@ namespace DMX.Core.Api.Brokers.Storages
             this.Database.Migrate();
         }
 
+        private async ValueTask<T> InsertAsync<T>(T @object)
+        {
+            this.Entry(@object).State = EntityState.Added;
+            await this.SaveChangesAsync();
+
+            return @object;
+        }
+
+        private IQueryable<T> SelectAll<T>() where T : class => this.Set<T>();
+
+        private async ValueTask<T> SelectAsync<T>(params object[] @objectIds) where T : class =>
+            await this.FindAsync<T>(objectIds);
+
+        private async ValueTask<T> UpdateAsync<T>(T @object)
+        {
+            this.Entry(@object).State = EntityState.Modified;
+            await this.SaveChangesAsync();
+
+            return @object;
+        }
+
+        private async ValueTask<T> DeleteAsync<T>(T @object)
+        {
+            this.Entry(@object).State = EntityState.Deleted;
+            await this.SaveChangesAsync();
+
+            return @object;
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+
             string connectionString =
                 this.configuration.GetConnectionString("DefaultConnection");
 
