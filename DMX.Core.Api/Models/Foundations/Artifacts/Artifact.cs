e1d1f71c63f6b7c9da4b185f465b8aec14df121f
Hassan Habib <hassanhabib@live.com>
2022-09-27 10:50:11 -0700

    CODE RUB: Upgrade Lab Command Broker to New Standard

--- DMX.Core.Api/Models/Foundations/Artifacts/Artifact.cs
+++ DMX.Core.Api/Models/Foundations/Artifacts/Artifact.cs
@@ -9,6 +9,6 @@ namespace DMX.Core.Api.Models.Foundations.Artifacts
     public class Artifact
     {
         public string Name { get; set; }
-        public Stream Content { get; set; }  
+        public Stream Content { get; set; }
     }
 }
