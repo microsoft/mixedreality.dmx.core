3dddd094928ba0ad39aafa35248f0cd52297a8fc
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2022-09-28 17:13:34 -0400

    ShouldThrowValidationExceptionOnAddIfArtifactIsInvalidAndLogItAsync -> FAIL

---
+++ DMX.Core.Api.Tests.Unit/Services/Foundations/Artifacts/ArtifactServiceTests.Validations.Add.cs
@@ -0,0 +1,104 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using DMX.Core.Api.Models.Foundations.Artifacts;
+using DMX.Core.Api.Models.Foundations.Artifacts.Exceptions;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace DMX.Core.Api.Tests.Unit.Services.Foundations.Artifacts
+{
+    public partial class ArtifactServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfNullAndLogItAsync()
+        {
+            // given
+            Artifact nullArtifact = null;
+            var nullArtifactException = new NullArtifactException();
+
+            var exptectedArtifactValidationException =
+                new ArtifactValidationException(nullArtifactException);
+
+            // when
+            ValueTask addArtifactTask =
+                this.artifactService.AddArtifactAsync(nullArtifact);
+
+            ArtifactValidationException actualArtifactValidationException =
+                await Assert.ThrowsAsync<ArtifactValidationException>(
+                    addArtifactTask.AsTask);
+
+            // then
+            actualArtifactValidationException.Should().BeEquivalentTo(
+                exptectedArtifactValidationException);
+
+            this.loggingBrokerMock.Verify(brokers =>
+                brokers.LogError(It.Is(SameExceptionAs(
+                    exptectedArtifactValidationException))),
+                        Times.Once);
+
+            this.artifactBroker.Verify(broker =>
+                broker.UploadArtifactAsync(It.IsAny<Artifact>()),
+                    Times.Never);
+
+            this.artifactBroker.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfArtifactIsInvalidAndLogItAsync(
+            string invalidString)
+        {
+            // given
+            var invalidArtifact = new Artifact
+            {
+                Name = invalidString,
+                Content = null
+            };
+
+            var invalidArtifactException = new InvalidArtifactException();
+
+            invalidArtifactException.AddData(
+                key: nameof(Artifact.Name),
+                values: "Text is required");
+
+            invalidArtifactException.AddData(
+                key: nameof(Artifact.Content),
+                values: "Content is required");
+
+            var expectedArtifactValidationException =
+                new ArtifactValidationException(invalidArtifactException);
+
+            // when
+            ValueTask addArtifactTask =
+                this.artifactService.AddArtifactAsync(invalidArtifact);
+
+            ArtifactValidationException actualArtifactValidationException =
+                await Assert.ThrowsAsync<ArtifactValidationException>(
+                    addArtifactTask.AsTask);
+
+            // then
+            actualArtifactValidationException.Should()
+                .BeEquivalentTo(expectedArtifactValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedArtifactValidationException))),
+                        Times.Once);
+
+            this.artifactBroker.Verify(broker =>
+                broker.UploadArtifactAsync(
+                    It.IsAny<Artifact>()),
+                        Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.artifactBroker.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ DMX.Core.Api/Models/Foundations/Artifacts/Exceptions/InvalidArtifactException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace DMX.Core.Api.Models.Foundations.Artifacts.Exceptions
+{
+    public class InvalidArtifactException : Xeption
+    {
+        public InvalidArtifactException() :
+            base(message: "Invalid artifact error occurred. Please fix the errors and try again.")
+        { }
+    }
+}
