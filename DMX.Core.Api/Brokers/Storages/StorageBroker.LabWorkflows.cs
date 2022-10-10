f1f4e30a8cd5d27ca961254ab865e0f66ced3d8d
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2022-10-10 16:49:27 -0400

    BROKERS: InsertLabWorkflow

--- DMX.Core.Api/Brokers/Storages/StorageBroker.LabWorkflows.cs
+++ DMX.Core.Api/Brokers/Storages/StorageBroker.LabWorkflows.cs
@@ -2,6 +2,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // ---------------------------------------------------------------
 
+using System.Threading.Tasks;
 using DMX.Core.Api.Models.Foundations.LabWorkflows;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,5 +11,8 @@ namespace DMX.Core.Api.Brokers.Storages
     public partial class StorageBroker
     {
         public DbSet<LabWorkflow> LabWorkflows { get; set; }
+
+        public async ValueTask<LabWorkflow> InsertLabWorkflowAsync(LabWorkflow labWorkflow) =>
+            await InsertAsync(labWorkflow);
     }
 }
