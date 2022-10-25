de5d0eec3253a87a0240c3ef1a64ce55af9e9eb2
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2022-10-25 16:31:51 -0400

    ShouldThrowOrchestrationServiceExceptionOnSubmitIfErrorOccursAndLogItAsync -> PASS

--- DMX.Core.Api.Tests.Unit/Services/Orchestrations/LabWorkflows/LabWorkflowOrchestrationServiceTests.Exceptions.Submit.cs
+++ DMX.Core.Api.Tests.Unit/Services/Orchestrations/LabWorkflows/LabWorkflowOrchestrationServiceTests.Exceptions.Submit.cs
@@ -2,6 +2,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // ---------------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using DMX.Core.Api.Models.Foundations.LabWorkflowCommands;
 using DMX.Core.Api.Models.Foundations.LabWorkflows;
@@ -107,6 +108,61 @@ namespace DMX.Core.Api.Tests.Unit.Services.Orchestrations.LabWorkflows
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogError(It.Is(SameExceptionAs(
                     expectedLabWorkflowOrchestrationDependencyException))),
+                        Times.Once);
+
+            this.labWorkflowCommandServiceMock.Verify(service =>
+                service.AddLabWorkflowCommandAsync(It.IsAny<LabWorkflowCommand>()),
+                    Times.Never);
+
+            this.labWorkflowEventServiceMock.Verify(service =>
+                service.AddLabWorkflowEventAsync(It.IsAny<LabWorkflow>()),
+                    Times.Never);
+
+            this.labWorkflowServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.labWorkflowCommandServiceMock.VerifyNoOtherCalls();
+            this.labWorkflowEventServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowOrchestrationServiceExceptionOnSubmitIfErrorOccursAndLogItAsync()
+        {
+            // given
+            LabWorkflow randomLabWorkflow = CreateRandomLabWorkflow();
+            LabWorkflow someLabWorkflow = randomLabWorkflow;
+            var exception = new Exception();
+
+            var failedLabWorkflowOrchestrationServiceException =
+                new FailedLabWorkflowOrchestrationServiceException(exception);
+
+            var expectedLabWorkflowOrchestrationServiceException =
+                new LabWorkflowOrchestrationServiceException(
+                    failedLabWorkflowOrchestrationServiceException);
+
+            this.labWorkflowServiceMock.Setup(service =>
+                service.AddLabWorkflowAsync(It.IsAny<LabWorkflow>()))
+                    .ThrowsAsync(exception);
+
+            // when
+            ValueTask<LabWorkflow> submitLabWorkflowTask =
+                this.labWorkflowOrchestrationService.SubmitLabWorkflowAsync(
+                    someLabWorkflow);
+
+            LabWorkflowOrchestrationServiceException actualLabWorkflowOrchestrationServiceException =
+                await Assert.ThrowsAsync<LabWorkflowOrchestrationServiceException>(
+                    submitLabWorkflowTask.AsTask);
+
+            // then
+            actualLabWorkflowOrchestrationServiceException.Should().BeEquivalentTo(
+                expectedLabWorkflowOrchestrationServiceException);
+
+            this.labWorkflowServiceMock.Verify(service =>
+                service.AddLabWorkflowAsync(It.IsAny<LabWorkflow>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedLabWorkflowOrchestrationServiceException))),
                         Times.Once);
 
             this.labWorkflowCommandServiceMock.Verify(service =>
