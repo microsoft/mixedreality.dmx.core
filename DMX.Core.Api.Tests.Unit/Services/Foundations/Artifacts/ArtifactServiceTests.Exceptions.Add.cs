861f51de89c67ccd065cb7b13827a0be0a0cee47
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2022-10-25 17:31:29 -0400

    ShouldThrowServiceExceptionOnAddIfErrorOccursAndLogItAsync -> FAIL

--- DMX.Core.Api.Tests.Unit/Services/Foundations/Artifacts/ArtifactServiceTests.Exceptions.Add.cs
+++ DMX.Core.Api.Tests.Unit/Services/Foundations/Artifacts/ArtifactServiceTests.Exceptions.Add.cs
@@ -2,6 +2,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // ---------------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using Azure;
 using DMX.Core.Api.Models.Foundations.Artifacts;
@@ -51,6 +52,49 @@ namespace DMX.Core.Api.Tests.Unit.Services.Foundations.Artifacts
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogError(It.Is(SameExceptionAs(
                     expectedArtifactDependencyException))),
+                        Times.Once);
+
+            this.artifactBroker.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnAddIfErrorOccursAndLogItAsync()
+        {
+            // given
+            Artifact randomArtifact = CreateRandomArtifact();
+            Artifact someArtifact = randomArtifact;
+            var serviceException = new Exception();
+
+            var failedArtifactServiceException =
+                new FailedArtifactServiceException(serviceException);
+
+            var expectedArtifactServiceException =
+                new ArtifactServiceException(failedArtifactServiceException);
+
+            this.artifactBroker.Setup(broker =>
+                broker.UploadArtifactAsync(It.IsAny<Artifact>()))
+                    .Throws(serviceException);
+
+            // when
+            ValueTask addArtifactTask =
+                this.artifactService.AddArtifactAsync(someArtifact);
+
+            ArtifactServiceException actualArtifactServiceException =
+                await Assert.ThrowsAsync<ArtifactServiceException>(
+                    addArtifactTask.AsTask);
+
+            // then
+            actualArtifactServiceException.Should().BeEquivalentTo(
+                expectedArtifactServiceException);
+
+            this.artifactBroker.Verify(broker =>
+                broker.UploadArtifactAsync(It.IsAny<Artifact>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedArtifactServiceException))),
                         Times.Once);
 
             this.artifactBroker.VerifyNoOtherCalls();
---
+++ DMX.Core.Api/Models/Foundations/Artifacts/Exceptions/ArtifactServiceException.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace DMX.Core.Api.Models.Foundations.Artifacts.Exceptions
+{
+    public class ArtifactServiceException : Xeption
+    {
+        public ArtifactServiceException(Xeption innerException)
+            : base(message: "Artifact service error occurred, contact support.",
+                  innerException)
+        { }
+    }
+}
