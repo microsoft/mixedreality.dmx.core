d846b32bc0c337a329ae2ab4bb9547c50796e1fe
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2022-11-11 15:32:23 -0500

    BROKERS: InsertLabDeviceAsync

--- DMX.Core.Api/Brokers/Storages/IStorageBroker.LabDevices.cs
+++ DMX.Core.Api/Brokers/Storages/IStorageBroker.LabDevices.cs
@@ -2,9 +2,13 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // ---------------------------------------------------------------
 
+using DMX.Core.Api.Models.Foundations.Labs;
+using System.Threading.Tasks;
+
 namespace DMX.Core.Api.Brokers.Storages
 {
     public partial interface IStorageBroker
     {
+        ValueTask<LabDevice> InsertLabDeviceAsync(LabDevice labDevice);
     }
 }
--- DMX.Core.Api/Brokers/Storages/StorageBroker.LabDevices.cs
+++ DMX.Core.Api/Brokers/Storages/StorageBroker.LabDevices.cs
@@ -2,6 +2,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // ---------------------------------------------------------------
 
+using System.Threading.Tasks;
 using DMX.Core.Api.Models.Foundations.Labs;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,5 +11,8 @@ namespace DMX.Core.Api.Brokers.Storages
     public partial class StorageBroker
     {
         public DbSet<LabDevice> LabDevices { get; set; }
+
+        public async ValueTask<LabDevice> InsertLabDeviceAsync(LabDevice labDevice) =>
+            await InsertAsync(labDevice);
     }
 }
