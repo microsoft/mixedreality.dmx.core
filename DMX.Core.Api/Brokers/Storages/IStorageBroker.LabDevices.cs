014f27890663cba0d2ffdca5953b716e15cd6348
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2022-11-11 15:38:34 -0500

    BROKERS: UpdateLabDeviceAsync

--- DMX.Core.Api/Brokers/Storages/IStorageBroker.LabDevices.cs
+++ DMX.Core.Api/Brokers/Storages/IStorageBroker.LabDevices.cs
@@ -2,9 +2,13 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // ---------------------------------------------------------------
 
+using System.Threading.Tasks;
+using DMX.Core.Api.Models.Foundations.Labs;
+
 namespace DMX.Core.Api.Brokers.Storages
 {
     public partial interface IStorageBroker
     {
+        ValueTask<LabDevice> UpdateLabDeviceAsync(LabDevice labDevice);
     }
 }
