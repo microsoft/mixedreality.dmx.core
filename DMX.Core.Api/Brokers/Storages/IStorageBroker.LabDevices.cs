da7b20556ca82bc18c106f52db26c5aca2503eb1
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2022-11-11 15:35:39 -0500

    BROKERS: SelectLabDeviceByIdAsync

--- DMX.Core.Api/Brokers/Storages/IStorageBroker.LabDevices.cs
+++ DMX.Core.Api/Brokers/Storages/IStorageBroker.LabDevices.cs
@@ -2,9 +2,14 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // ---------------------------------------------------------------
 
+using System;
+using System.Threading.Tasks;
+using DMX.Core.Api.Models.Foundations.Labs;
+
 namespace DMX.Core.Api.Brokers.Storages
 {
     public partial interface IStorageBroker
     {
+        ValueTask<LabDevice> SelectLabDeviceByIdAsync(Guid labDeviceId);
     }
 }
--- DMX.Core.Api/Brokers/Storages/StorageBroker.LabDevices.cs
+++ DMX.Core.Api/Brokers/Storages/StorageBroker.LabDevices.cs
@@ -2,6 +2,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // ---------------------------------------------------------------
 
+using System;
+using System.Threading.Tasks;
 using DMX.Core.Api.Models.Foundations.Labs;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,5 +12,8 @@ namespace DMX.Core.Api.Brokers.Storages
     public partial class StorageBroker
     {
         public DbSet<LabDevice> LabDevices { get; set; }
+
+        public async ValueTask<LabDevice> SelectLabDeviceByIdAsync(Guid labDeviceId) =>
+            await FindAsync<LabDevice>(labDeviceId);
     }
 }
