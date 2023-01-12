38db07db37175ac738e46937c768e78a001c7160
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2023-01-12 11:49:32 -0500

    ShouldThrowDependencyValidationExceptionOnAddIfLabArtifactAlreadyExistsAndLogItAsync -> FAIL

---
+++ DMX.Core.Api/Models/Foundations/LabArtifacts/Exceptions/LabArtifactDependencyValidationException.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace DMX.Core.Api.Models.Foundations.LabArtifacts.Exceptions
+{
+    public class LabArtifactDependencyValidationException : Xeption
+    {
+        public LabArtifactDependencyValidationException(Xeption innerException)
+            : base(message: "Lab artifact dependency validation error occurred, please contact support.",
+                  innerException)
+        { }
+    }
+}
