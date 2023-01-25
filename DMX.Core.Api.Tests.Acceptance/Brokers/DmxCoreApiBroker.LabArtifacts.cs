f1a6aab29332c812bea9fe66d1e00096a017c9b0
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2023-01-25 12:55:16 -0500

    ACCEPTANCE: POST Lab Artifact

---
+++ DMX.Core.Api.Tests.Acceptance/Brokers/DmxCoreApiBroker.LabArtifacts.cs
@@ -0,0 +1,21 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using DMX.Core.Api.Models.Foundations.LabArtifacts;
+
+namespace DMX.Core.Api.Tests.Acceptance.Brokers
+{
+    public partial class DmxCoreApiBroker
+    {
+        private const string LabArtifactsApiRelativeUrl = "api/labartifacts";
+        private const string LabArtifactsMediaType = "application/octet-stream";
+
+        public async ValueTask PostLabArtifactAsync(LabArtifact labArtifact) =>
+            await this.apiFactoryClient.PostContentWithNoResponseAsync(
+                relativeUrl: $"{LabArtifactsApiRelativeUrl}/{labArtifact.Name}",
+                content: labArtifact.Content,
+                mediaType: LabArtifactsMediaType);
+    }
+}
